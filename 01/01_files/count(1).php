f13fa8ee3eb52984d846a7e1ddd2f5438d3c22e7
jash-git <jash.liao@gmail.com>
2018-04-18 13:33:47 +0800

    Create

---
+++ 01/01_files/count(1).php
@@ -0,0 +1,7 @@
+
+		document.getElementById("usercount").innerHTML="2";
+		document.getElementById("totalvisit").innerHTML="146787";
+		document.getElementById("totalvisit_ip").innerHTML="88092";
+		document.getElementById("todayvisit").innerHTML="7";
+		document.getElementById("todayvisit_ip").innerHTML="6";
+	
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+﻿Windows https apache php 環境架設教學
+
+01.
+	啟動 Windows 版 Apache 的 HTTPS (SSL) 功能
+	資料來源:http://neidi.homeip.net/blog/cblog/index.php?id=414
+	
+02.
+	在 windows 環境底下設定 apache ( 32 位元 ) SSL 憑證讓網址變成 https
+	資料來源:http://icodding.blogspot.tw/2015/10/windows-apache-32-ssl-https.html
+	
